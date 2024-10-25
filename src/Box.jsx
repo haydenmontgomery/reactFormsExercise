104d16975d3e703e56a437c6cb458778774990df
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-10-24 23:00:55 -0400

    Finished proper box placement

--- src/Box.jsx
+++ src/Box.jsx
@@ -3,20 +3,31 @@ import "./Box.css";
 
 const Box = ({idx, width, height, bgcolor, deleteBox}) => {
   return (
-    
+    <div style={{
+      position: "relative",
+      marginBottom: "20px"
+    }}>
+      <button
+        style={{
+        position: "absolute",
+        top: "10px",
+        left: "0px"
+        }}
+      onClick={() => deleteBox(idx)}
+      >
+        Delete!
+      </button>
     <div
       className="Box"
       style={{
         backgroundColor: bgcolor,
         width: `${width}px`,
         height: `${height}px`,
+        position: "absoluute",
+        left: "100px"
       }}
     >
-      <button
-      onClick={() => deleteBox(idx)}
-      >
-        Delete!
-      </button>
+    </div>
     </div>
   )
 
